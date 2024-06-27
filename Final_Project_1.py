a567c75313b8b60c7ec6b9f99af2855c65f46b8a
Ramiro Angulo <lascivioligamen@gmail.com>
2024-06-27 13:04:20 -0500

    Add files via upload

---
+++ Final_Project_1.py
@@ -0,0 +1,127 @@
+#Ramiro Angulo
+#Final Project
+#DSA 5303
+
+
+#IMPORT LIBRARIES
+import pandas as pd
+import numpy as np
+import yfinance as yf
+import matplotlib.pyplot as plt
+
+#The purpose of this application is to develop two programs for the following financial models: CAPM and Monte Carlo Simulation.
+
+
+#CAPITAL ASSET PRICING MODEL
+
+# importing the historical stock data BA The Boeing Company
+stock_data = yf.download('BA', start='2020-01-01', end='2024-01-01')
+# importing the historial market data ^GSPC S&P 500 Index
+market_data = yf.download('^GSPC', start='2020-01-01', end='2024-01-01')
+# Calculate the log daily returns for stock and market data
+stock_data['Returns'] = np.log(stock_data['Adj Close']) - np.log(stock_data['Adj Close'].shift(1))
+market_data['Market Returns'] = np.log(market_data['Adj Close']) - np.log(market_data['Adj Close'].shift(1))
+#plot log market returns
+plt.figure(figsize=(10,5))
+plt.plot(market_data)
+plt.title('^GSPC')
+plt.xlabel('Time (days)')
+plt.ylabel('Return')
+plt.show()
+#plot log stock returns
+plt.figure(figsize=(10,5))
+plt.plot(stock_data)
+plt.title('BA')
+plt.xlabel('Time (days)')
+plt.ylabel('Return')
+plt.show()
+#caclulate the expected rate of return for stock log returns
+expected_stock_ror = stock_data['Returns'].mean()
+#calculate the expected rate of return for market log returns
+expected_market_ror = market_data['Market Returns'].mean()
+risk_free_rate = 5.5  #3 Month U.S. Treasury Bill Rate 5.5% risk-free rate as of 26 June 2024
+# create dataframe which contains both Stock and Market expected rates of return
+# drop na values to allow for covariance and variacne calculations
+returns = pd.DataFrame({
+    'Stock': stock_data['Returns'],
+    'Market': market_data['Market Returns']
+}).dropna()
+# create a covariance matrix from the returns dataframe
+cov_matrix = returns.cov()
+# retrieve the covariance between Stock and Market from the covariance matrix
+stock_market_cov = cov_matrix.loc['Stock', 'Market']
+#caclulate the Market variance
+market_var = returns['Market'].var()
+# using the covariance between Stock and Market, cacluate the beta value.
+beta = (stock_market_cov) / (market_var)
+print("The Beta of the stock is: ", beta)
+#Using the CAPM formula, calculate the expected rate of return of the stock
+expected_return_capm = risk_free_rate + beta * (expected_market_ror - risk_free_rate)
+#view result
+print(f"Expected Return based on CAPM: {expected_return_capm}")
+
+
+#MONTE CARLO SIMULATION
+#https://www.researchgate.net/publication/358954203_Monte_Carlo_Simulation_Prediction_of_Stock_Prices
+
+# Parameters
+S0 = stock_data['Adj Close'][-1]
+T = 1  # Time period (1 year)
+N = 252  # Number of time steps (trading days)
+dt = T/N
+mu = expected_stock_ror
+sigma = stock_data['Returns'].std()
+
+# Simulation
+np.random.seed(42)
+simulations = 1000
+price_i = np.zeros((N, simulations))
+price_i[0] = S0
+price_i_mean = np.zeros(simulations)
+#calculate the price simulations
+for t in range(1, N):
+    z = np.random.standard_normal(simulations)
+    price_i[t] = price_i[t-1] * np.exp((mu - 0.5 * sigma**2) * dt + sigma * np.sqrt(dt) * z)    
+#convert to dataframe
+price_i = pd.DataFrame(price_i) 
+
+#caclulate each simulation's mean
+for i in range(price_i.shape[1]):
+    price_i_mean[i] = price_i.iloc[:, i].mean()
+#convert to dataframe
+price_i_mean = pd.DataFrame(price_i_mean)
+#create price rolling mean data frame
+prm = pd.DataFrame(np.zeros(simulations))
+#caclulate the rolling mean of the simulation means
+for i in range(price_i_mean.shape[0]):
+    prm[i] = price_i_mean.rolling(i).mean()
+#print(prm.iloc[simulations-1, 0:])
+
+#plot the simulations
+plt.figure(figsize=(10,5))
+plt.plot(price_i)
+plt.title('Monte Carlo Simulation of Stock Prices')
+plt.xlabel('Time (days)')
+plt.ylabel('Stock Price')
+plt.show()
+#plot the rolling mean of the simulations
+plt.figure(figsize=(10,5))
+plt.plot(prm.iloc[simulations-1, 0:])
+plt.title('Monte Carlo Simulation Rolling Mean')
+plt.xlabel('Simulation Count')
+plt.ylabel('Stock Price')
+plt.show()
+
+#retrieve final prices
+#final_prices = prm.iloc[:,-1]
+final_prices = price_i.iloc[-1,:]
+#caclulte the mean 
+mean_final_price = final_prices.mean()
+#cacluate forcasted expected rate of return using the simulated prices
+# Calculate the returns for each path
+print("Expected Rate of Return for Monte Carlo Simulation: ", ((final_prices - S0) / S0).mean())
+#caculate the 97% confidence interval
+confidence_interval = np.percentile(final_prices, [3, 97])
+#print results
+print("Mean final stock price: ", mean_final_price )
+print("97% confidence interval: ", confidence_interval)
