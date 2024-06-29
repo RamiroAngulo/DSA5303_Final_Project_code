f96899bf504fa792c4c6e65417c5257edbb77ce4
Ramiro Angulo <lascivioligamen@gmail.com>
2024-06-29 11:35:28 -0500

    Update Final_Project.py

--- Final_Project.py
+++ Final_Project.py
@@ -10,13 +10,15 @@ import matplotlib.pyplot as plt
 
 #The purpose of this application is to develop two programs for the following financial models: CAPM and Monte Carlo Simulation.
 
+
 #CAPITAL ASSET PRICING MODEL
 
 # importing the historical stock data BA The Boeing Company
-stock_data = yf.download('BA', start='2020-01-01', end='2024-01-01')
-#print(stock_data)
+stock_data = yf.download('BA', start='2022-01-01', end='2023-01-01')
+# import stock for a year later on the day
+stock_year = yf.download('BA', start='2024-01-01', end='2024-01-05')
 # importing the historial market data ^GSPC S&P 500 Index
-market_data = yf.download('^GSPC', start='2020-01-01', end='2024-01-01')
+market_data = yf.download('^GSPC', start='2022-01-01', end='2023-01-01')
 #print(market_data)
 # Calculate the log daily returns for stock data
 #assign to new Column named Returns
@@ -28,14 +30,14 @@ market_data['Market Returns'] = np.log(market_data['Adj Close']) - np.log(market
 #plot log market returns
 plt.figure(figsize=(10,5))
 plt.plot(market_data)
-plt.title('^GSPC')
+plt.title('S&P 500 Index Return')
 plt.xlabel('Time (days)')
 plt.ylabel('Return')
 plt.show()
 #plot log stock returns
 plt.figure(figsize=(10,5))
 plt.plot(stock_data)
-plt.title('BA')
+plt.title('The Boeing Company Return')
 plt.xlabel('Time (days)')
 plt.ylabel('Return')
 plt.show()
@@ -46,13 +48,12 @@ expected_stock_ror = stock_data['Returns'].mean()
 #calculate the expected rate of return for market log returns
 expected_market_ror = market_data['Market Returns'].mean()
 #print(expected_market_ror)
-risk_free_rate = 5.5  #3 Month U.S. Treasury Bill Rate 5.5% risk-free rate as of 26 June 2024
-# create dataframe which contains both Stock and Market expected rates of return
+risk_free_rate = .0509  #1 Year Treasury Rate 5.09% risk-free rate as of 28 June 2024 
+# create dataframe which contains both Stock and Market logorithmic returns
 # drop na values to allow for covariance and variacne calculations
-returns = pd.DataFrame({
-    'Stock': stock_data['Returns'],
-    'Market': market_data['Market Returns']
-}).dropna()
+returns = pd.DataFrame(pd.concat([stock_data['Returns'], market_data['Market Returns']], axis=1).dropna())
+#rename columns for unique identifiers to alleviate confusion in the code
+returns = returns.rename(columns={'Returns': 'Stock', 'Market Returns': 'Market'})
 #with pd.option_context('display.max_rows', None, 'display.max_columns', None): 
 #    print(returns)
 # create a covariance matrix from the returns dataframe
@@ -71,16 +72,17 @@ print("The Beta of the stock is: ", beta)
 expected_return_capm = risk_free_rate + beta * (expected_market_ror - risk_free_rate)
 #view result
 print("Expected Return based on CAPM:", expected_return_capm)
-
+#using the estimated rate of return, calculate estimated final price
+print("Expected Price based on CAPM:", stock_data['Adj Close'][-1]*(expected_return_capm+1))
 
 #MONTE CARLO SIMULATION
 
 # PARAMETERS
 #intilize stock price using last price of historial stock data
 S0 = stock_data['Adj Close'][-1]
-T = 1  # Total time duration
+T = 1  # Total time interval
 N = 252  # step increment
-dt = T/N # time increment
+dt = T/N # will allow a 252 trading day year worth of price predictions
 mu = expected_stock_ror #pull expected stock rate of return
 sigma = stock_data['Returns'].std() #standard deviation of target stock returns
 
@@ -132,10 +134,25 @@ final_prices = price_i.iloc[-1,:]
 mean_final_price = final_prices.mean()
 #cacluate forcasted expected rate of return using the simulated prices
 # Calculate the returns for each path
-expected_return_monte = ((final_prices - S0) / S0).mean()
+#expected_return_monte = ((final_prices - S0) / S0).mean()
+expected_return_monte = ((mean_final_price - S0) / S0)
 print("Expected Rate of Return for Monte Carlo Simulation: ", expected_return_monte)
 #caculate the 97% confidence interval
 confidence_interval = np.percentile(final_prices, [3, 97])
 #print results
 print("Mean final stock price: ", mean_final_price )
 print("97% confidence interval: ", confidence_interval)
+
+#retrieve the actual stock price a year later
+print("Actual Price a year later: ", stock_year["Adj Close"][0])
+#calculate the actual rate of return 
+print("Actual rate of return a year later:", (stock_year["Adj Close"][0] - S0)/S0)
+
+
+#caculate different confidence intervals
+#confidence_interval_test1 = np.percentile(final_prices, [5, 95])
+#print("95% confidence interval: ", confidence_interval_test1)
+#confidence_interval_test2 = np.percentile(final_prices, [10, 90])
+#print("90% confidence interval: ", confidence_interval_test2)
+#confidence_interval_test3 = np.percentile(final_prices, [15, 85])
+#print("85% confidence interval: ", confidence_interval_test3)
